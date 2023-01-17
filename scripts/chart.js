ec27fb3a2d014da8a2dc3c0cd11638e55a139eb3
Rifqi <2072006@maranatha.ac.id>
2023-01-17 11:13:33 +0700

    updet portfolio

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,4 +1,5 @@
 const canvasChart = document.querySelector('#martin-skills-chart');
+const kipChart = document.querySelector('#kipskills');
 
 let config = {
   type: 'bar',
@@ -30,3 +31,31 @@ let config = {
 };
 
 let chart = new Chart(canvasChart, config);
+
+let config1 = {
+  type: 'bar',
+  data: {
+    labels: ['C#', 'Unity', 'Flutter', 'Javascript'],
+    datasets: [
+      {
+        label: 'Skills Rate',
+        data: [80, 90, 80, 75],
+        backgroundColor: [
+          'rgba(255, 159, 64, 0.2)', //1
+          'rgba(54, 162, 235, 0.2)', //2
+          'rgba(255, 159, 64, 0.2)', //3
+          'rgba(54, 162, 235, 0.2)', //4
+        ],
+        borderColor: [
+          'rgba(255, 159, 64, 1)', //1
+          'rgba(54, 162, 235, 1)', //2
+          'rgba(255, 159, 64, 1)', //3
+          'rgba(54, 162, 235, 1)', //4
+        ],
+        borderWidth: 1,
+      }
+    ]
+  }
+}
+
+let rifqiChart = new Chart(kipChart, config1)
